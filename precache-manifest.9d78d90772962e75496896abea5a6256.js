95bbef4f18424546384f8a88f270ade8ecd41472
JacksWebBrand <jbizzell@protonmail.com>
2020-08-29 01:13:14 +0100

    Updates

---
+++ precache-manifest.9d78d90772962e75496896abea5a6256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9962201b1e543e264459a90d7c2f4bd5",
+    "url": "/rf/index.html"
+  },
+  {
+    "revision": "d8432c5fa015efa899b8",
+    "url": "/rf/static/css/main.008bd878.chunk.css"
+  },
+  {
+    "revision": "2d0c70cbb82cbfc6dc3c",
+    "url": "/rf/static/js/2.4e0d3651.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rf/static/js/2.4e0d3651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8432c5fa015efa899b8",
+    "url": "/rf/static/js/main.227a074f.chunk.js"
+  },
+  {
+    "revision": "d1f1b524d669c4db8fcc",
+    "url": "/rf/static/js/runtime-main.6dc7695e.js"
+  }
+]);
